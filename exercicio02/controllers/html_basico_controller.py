e90ab5caa15cedf738f76fe64880a8fa4d6ddd7b
FERNANDO RIBEIRO RODRIGUES <06861511005@senacrs.edu.br>
2026-07-22 10:22:57 -0300

    feat: Finaliza o exercicio 02

--- exercicio02/controllers/html_basico_controller.py
+++ exercicio02/controllers/html_basico_controller.py
@@ -3,9 +3,12 @@ from flask import render_template
 class htmlbasico(BaseController):
     def __init__(self,app):
         self.rotas = [
-            ('/','home',self.pagina_inicial)
+            ('/','home',self.pagina_inicial),
+            ('/cards','cards',self.pagina_cards)
         ]
         super().__init__(app)
 
     def pagina_inicial(self):
         return render_template("pagina_inicial.html")
+    def pagina_cards(self):
+        return render_template("cards.html")
