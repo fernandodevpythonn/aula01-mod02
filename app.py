6b5c1069d509c56c3de2ed9b8066723976def7a7
FERNANDO RIBEIRO RODRIGUES <06861511005@senacrs.edu.br>
2026-08-10 11:54:35 -0300

    fix: resolvendo problema do render-template

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from controllers.html_basico_controller import HtmlBasico_controller
+from desenvolvimento_interface_lista01.exercicio01.controllers.html_basico_controller01 import HTMLBasicoController01
 app = Flask(__name__)
 HtmlBasico_controller(app)
-
+HTMLBasicoController01(app)
 if __name__ == "__main__":
   app.run(debug=True)
