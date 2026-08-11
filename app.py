3553a8444ed127bc59f66e6e22dd5bc0bdf21139
fernandodevpythonn <fernandoribeiro270806@gmail.com>
2026-08-11 00:13:34 -0300

    feat: Fazendo a criacao do dropdown

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from controllers.html_basico_controller import HtmlBasico_controller
-from desenvolvimento_interface_lista01.exercicio01.controllers.html_basico_controller01 import HTMLBasicoController01
+from lista01.exercicio01.controllers.html_basico_controller01 import HTMLBasicoController01
 app = Flask(__name__)
 HtmlBasico_controller(app)
 HTMLBasicoController01(app)
